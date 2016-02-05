5e8abe73aba5feac62f421cbac56dd06348a7c42
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-02-05 10:30:29 -0330

    Improve flag description.

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,6 @@ var RootCmd = &cobra.Command{Use: "clowder"}
 
 func init() {
 	var flags = RootCmd.PersistentFlags()
-	flags.IntVarP(&tcpPort, "port", "p", 5000, "TCP port to connect")
+	flags.IntVarP(&tcpPort, "port", "p", 5000, "TCP control port")
 
 }
