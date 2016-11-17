ef07042fa0cee70d11d768adf965a4e53f9fd3c9
scu677 <scu677@mun.ca>
2016-11-17 14:04:12 -0330

    Second commit

--- ClowderApp/app.go
+++ ClowderApp/app.go
@@ -29,22 +29,24 @@ func checkErr(err error) {
 		panic(err)
 	}
 }
-
 type machine struct {
 	Name              string
+	MACaddress        string
 	Architecture      string
-	MicroArchitecture string
-	MemorySize        int
-	pxe               string
-	nfsroot           string
-}
+	Microarchitecture string
+	MemoreySize        int
+	Pxe               string
+	Nfsroot           string
+
+	}
 type reservation struct {
-	user    string
-	machine string
-	start   int
-	end     int
-	ended   int
+	User    string
+	Machine string
+	Start   int
+	End     int
+	Ended   int
 }
+
 type user struct {
 	name     string
 	username string
@@ -53,16 +55,7 @@ type user struct {
 }
 type disk struct {
 }
-
-// creating function that get machines details
-func getmachine(detail *machine) {
-	db, err := sql.Open("sqlite3", "List.db")
-	checkErr(err)
-	rows, err := db.Query("SELECT *FROM machines")
-	checkErr(err)
-	defer rows.Close()
-
-}
+//create function for each page struc and query table, then parse it to the templates.
 func output(w http.ResponseWriter, r *http.Request) {
 
 	db, err := sql.Open("sqlite3", "List.db")
@@ -71,18 +64,26 @@ func output(w http.ResponseWriter, r *http.Request) {
 	checkErr(err)
 	defer rows.Close()
 	machines := []machine{}
+	//details := []machine{}
 	for rows.Next() {
 		var c machine
-		err = rows.Scan(&c.Id, &c.Name, &c.MemorySize, &c.Status)
+		//err = rows.Scan(&c.Id, &c.Name, &c.MemorySize, &c.Status)
+		err = rows.Scan(&c.Name, &c.MACaddress, &c.Architecture, &c.Microarchitecture, &c.MemoreySize, &c.Pxe, &c.Nfsroot)
 		checkErr(err)
 		machines = append(machines, c)
-	}
-	t, _ := template.ParseFiles("interface/mylayout.html")
-	t.Execute(w, machines)
-}
+		//details = append(details, c)
 
+	}
+    t, _ :=template.ParseFiles("interface/mylayout.html")
+    t.Execute(w, machines)
+    ts, _:=template.ParseFiles("interface/computer.html")
+    ts.Execute(w, machines)
+  }
+//create handler for each function
 func main() {
-	http.HandleFunc("/", output)
+	fd := http.FileServer(http.Dir("interface"))
+	http.Handle("/",fd)
+	http.HandleFunc("/mylayout.html", output)
 	log.Println("Loading....")
-	http.ListenAndServe(":8010", nil)
+	http.ListenAndServe(":8080", nil)
 }
