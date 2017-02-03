cfe46c980f206101edecdcb78b40e43c4b4df6e8
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-02-03 20:18:01 -0330

    Prefix all titles with "Clowder: ".

--- src/html/bootstrap.rs
+++ src/html/bootstrap.rs
@@ -43,7 +43,7 @@ pub fn render<S>(title: S, ctx: &Context, flash: Option<FlashMessage>, content: 
                 meta charset="utf-8" /
                 meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /
 
-                title (title.into())
+                title (format!["Clowder: {}", title.into()])
 
                 link rel="stylesheet" href="/static/css/bootstrap.min.css" /
                 link rel="stylesheet" href="/static/css/musec.css" /
