979802263e7834a17902a7026bdac363d2e96ab8
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-02-07 18:17:00 -0330

    Remove a superfluous jQuery import.
    Apparently we were fetching jQuery twice, once with integrity properties and once without!

--- src/html/bootstrap.rs
+++ src/html/bootstrap.rs
@@ -170,7 +170,6 @@ impl Render for Page {
                         }
                     }
 
-                    script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" {}
                     script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
                            integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
                            crossorigin="anonymous" {}
