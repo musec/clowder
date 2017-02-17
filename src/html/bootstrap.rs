8f58d45642d0aa3a7f7538744505fbbd07d7f10f
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-02-17 15:31:56 -0330

    Import jQuery earlier.
    This allows us to use jQuery constructs within the body of a page.

--- src/html/bootstrap.rs
+++ src/html/bootstrap.rs
@@ -109,6 +109,10 @@ impl Render for Page {
                     link rel="stylesheet" href="/static/css/musec.css" /
                     link rel="stylesheet" href="/static/css/sticky-footer-navbar.css" /
                     link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" /
+
+                    script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+                           integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+                           crossorigin="anonymous" {}
                 }
 
                 body {
@@ -170,10 +174,6 @@ impl Render for Page {
                             }
                         }
                     }
-
-                    script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
-                           integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
-                           crossorigin="anonymous" {}
 
                     script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
                            integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
