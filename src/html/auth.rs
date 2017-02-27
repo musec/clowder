97d6661d085ed04bdcd029e0755f86c9464aac3f
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-02-27 15:08:14 -0330

    Add cookie integrity checking.
    If a cookie is going to act as a bearer token, we had better authenticate it!

--- src/html/auth.rs
+++ src/html/auth.rs
@@ -1,18 +1,44 @@
+use crypto::hmac::Hmac;
+use crypto::mac::Mac;
+use crypto::sha2::Sha512;
 use db::models::*;
 use db::schema::*;
 use diesel::*;
 use diesel::pg::PgConnection as Connection;
+use rand::Rng;
+use rand::os::OsRng;
 use rocket::http::{Cookie,CookieJar};
+use rustc_serialize::hex::ToHex;
 
 use html::error::Error;
 
+use std::io;
+
+lazy_static! {
+    /// A random HMAC key that is regenerated on every server restart.
+    static ref HMAC_KEY: [u8; 32] = OsRng::new().expect("Unable to open system RNG").gen();
+}
 
 /// Authenticate a user request, returning either a User or an Error.
 pub fn authenticate(jar: &CookieJar, conn: &Connection) -> Result<User, Error> {
-    jar.find("username")
+    jar.find("user")
         .map(|cookie| cookie.value().to_string())
         .ok_or(Error::AuthRequired)
-        .and_then(|ref uname| {
+        .and_then(|value| {
+            let idx = value.rfind("-").ok_or(
+                Error::AuthError(format!["no mac found in cookie: {}", value]))?;
+
+            let (uname, mac) = value.split_at(idx);
+            let my_mac = hmac(uname)?;
+
+            if my_mac != mac[1..] {
+                println!["Cookie authentication failure: {} != {}", my_mac, &mac[1..]];
+                return Err(Error::AuthRequired);
+            }
+
+            Ok(uname.to_string())
+        })
+        .and_then(|uname| {
             use self::users::dsl::*;
 
             users.filter(username.eq(uname))
@@ -27,6 +53,18 @@ pub fn logout<'c>(jar: &CookieJar) {
 }
 
 /// Generate a cookie that attests to a logged-in user's username.
-pub fn set_user_cookie<'c, S: Into<String>>(jar: &CookieJar, username: S) {
-    jar.add(Cookie::new(String::from("username"), username.into()))
+pub fn set_user_cookie<'c, S: Into<String>>(jar: &CookieJar, username: S)
+    -> Result<(), Error>
+{
+    let name = username.into();
+    let value = format!["{}-{}", name, hmac(&name)?];
+    jar.add(Cookie::new(String::from("user"), value));
+
+    Ok(())
+}
+
+fn hmac(input: &str) -> Result<String, Error> {
+    let mut hmac = Hmac::new(Sha512::new(), &*HMAC_KEY);
+    hmac.input(input.as_bytes());
+    Ok(hmac.result().code().to_hex())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@
 
 extern crate chrono;
 extern crate chrono_humanize;
+extern crate crypto;
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate diesel_codegen;
 extern crate dotenv;
 extern crate hyper;
 extern crate hyper_native_tls;
+#[macro_use] extern crate lazy_static;
 extern crate maud;
 extern crate marksman_escape;
 extern crate native_tls;
+extern crate rand;
 extern crate rocket;
 extern crate rustc_serialize;
 extern crate url;
