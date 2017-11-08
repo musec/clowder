5feb72b833dc0456d3dc67dfe8a99f84c23037a5
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-11-08 14:41:55 -0330

    Add project metadata and copyright information.

--- src/db/models.rs
+++ src/db/models.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2016-2017 Jonathan Anderson
+ *
+ * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+ * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+ * http://opensource.org/licenses/MIT>, at your option. This file may not be
+ * copied, modified, or distributed except according to those terms.
+ */
+
 use chrono::{DateTime,Utc};
 use db::schema::*;
 use diesel;
