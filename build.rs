1eff35d5d41cfe9243a93c806713b572009a1a4b
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-11-08 09:59:21 -0330

    Handle potential `git describe` error(s).

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 extern crate vergen;
 
+use std::io::{self, Write};
+
 fn main() {
-    vergen::vergen(vergen::SEMVER);
+    vergen::vergen(vergen::SEMVER)
+           .or_else(|err| writeln![io::stderr(), "unable to generate version string: {:?}", err])
+           .expect("failed to print error message for version string generation error");
 }
