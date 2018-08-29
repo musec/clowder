3c3d8e7ec24008003a7f69de5347e76b1a9db203
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-08-28 21:04:08 -0400

    FIXUP Maud thing

--- src/html/tables.rs
+++ src/html/tables.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Jonathan Anderson
+ * Copyright 2017-2018 Jonathan Anderson
  *
  * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
  * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
